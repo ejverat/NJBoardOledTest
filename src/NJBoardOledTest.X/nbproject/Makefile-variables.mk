09892f268f8bc3a55872cf9c7c7c4552c081cb16
evera <emm.vera@gmail.com>
2021-10-26 00:45:16 -0500

    Created basic project and documentation

---
+++ src/NJBoardOledTest.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=NJBoardOledTest.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/NJBoardOledTest.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=njboardoledtest.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/njboardoledtest.x.tar
